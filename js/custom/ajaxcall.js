cc042c7cfeb14509637e93efb1558711a94fc6a3
Komal97 <komalbansal97@gmail.com>
2018-01-28 13:10:01 +0530

    initial commit

---
+++ js/custom/ajaxcall.js
@@ -0,0 +1,12 @@
+const ajaxcall={
+    ajax(fn,url,key){
+        var xmlhttprqst=new XMLHttpRequest();
+        xmlhttprqst.open("GET",url);
+        xmlhttprqst.onreadystatechange=function(){
+            if(xmlhttprqst.readyState==4 && xmlhttprqst.status==200){
+              fn(xmlhttprqst.responseText,key);
+            }
+        }
+        xmlhttprqst.send(null);  
+    }
+}
---
+++ js/custom/cartOperations.js
@@ -0,0 +1,24 @@
+var price=0;
+var total=0;
+//var cartquantity=0;
+const cartOperations={
+    itemList:[],
+    add(currentobject){
+        this.itemList.push(currentobject);
+    },
+
+    // calcQuantity(quantity){
+    //    return cartquantity=quantity-1;
+    // },
+
+    calcPrice(totalprice){
+        return price=price+totalprice;
+    },
+    calcCartSubtotal(cartprice){
+        return total=cartprice + (cartprice*0.1); 
+    },
+
+    deleteItem(){
+        return this.itemList.splice(0,this.itemList.length);
+    }
+}
